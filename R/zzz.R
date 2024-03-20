1b96cbd4ceb371b44b4cf21559ffe2f510b15ebe
ecmerkle <merklee@missouri.edu>
2024-03-20 13:21:29 -0500

    nuke everything that blavaan will handle

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,5 @@
     version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
                       fields="Version")
     packageStartupMessage("This is ",paste(pkgname, version))
-    packageStartupMessage('On multicore systems, we suggest use of future::plan("multicore") or\n', '  future::plan("multisession") for faster post-MCMC computations.')
 }
 
--- tests/tinytest.R
+++
@@ -1,5 +0,0 @@
-## test only if it is a development version
-if (length(strsplit(packageDescription("blavaan")$Version, "\\.")[[1]]) > 2 &
-    requireNamespace("tinytest", quietly=TRUE)) {
-  tinytest::test_package("blavaan")
-}
